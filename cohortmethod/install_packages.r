d5b3334bfc3c69174a5a5d9870a82f6c64366acb
chandryou <applegna@gmail.com>
2019-02-06 20:45:07 +0900

    first draft of docker image for cohortmethod 3.0.2

---
+++ cohortmethod/install_packages.r
@@ -0,0 +1,7 @@
+devtools::install_github("ohdsi/SqlRender", ref = "v1.5.2")
+devtools::install_github("ohdsi/DatabaseConnector", ref = "v2.2.0")
+devtools::install_github("ohdsi/OhdsiSharing", ref = "v0.1.3")
+devtools::install_github("ohdsi/FeatureExtraction", ref = "v2.1.5")
+devtools::install_github("ohdsi/CohortMethod", ref = "v3.0.2")
+devtools::install_github("ohdsi/EmpiricalCalibration", ref = "v1.3.6")
+devtools::install_github("ohdsi/MethodEvaluation", ref = "v0.3.1")
