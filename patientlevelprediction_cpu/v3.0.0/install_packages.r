1e2dc7869aad2b43246b4fde5d197e9c6837ea14
chandryou <applegna@gmail.com>
2019-02-12 18:20:59 +0900

    install plp package in the docker image

--- patientlevelprediction_cpu/v3.0.0/install_packages.r
+++ patientlevelprediction_cpu/v3.0.0/install_packages.r
@@ -5,10 +5,15 @@ install.packages("keras")
 #tensorflow::install_tensorflow()
 #keras::install_keras()
 
+#install ohdsi tools
 devtools::install_github("ohdsi/SqlRender", ref = "v1.5.2")
 devtools::install_github("ohdsi/DatabaseConnector", ref = "v2.2.0")
 devtools::install_github("ohdsi/OhdsiSharing", ref = "v0.1.3")
 devtools::install_github("ohdsi/FeatureExtraction", ref = "v2.1.5")
 devtools::install_github("ohdsi/OhdsiRTools", ref= "v1.7.0")
 devtools::install_github("ohdsi/BigKnn")
-#devtools::install_github("ohdsi/PatientLevelPrediction", ref="v3.0.0")
+
+#install pythonInR and PLP packages
+Sys.setenv('USESPECIALPYTHONVERSION'='python3.5')
+install.packages("PythonInR")
+devtools::install_github("ohdsi/PatientLevelPrediction", ref="v3.0.0")
